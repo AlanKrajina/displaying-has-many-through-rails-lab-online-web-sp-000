9d77407fa5cfb472ea64edafc803cd44f44b08cc
Alan Krajina <alankrajina88@gmail.com>
2019-08-15 11:33:54 +0000

    Automatically backed up by Learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,10 @@
 class PatientsController < ApplicationController
+
+  def index
+      @patients = Doctor.find(params[:id])
+  end
+
+  def show
+      @patient = Patient.find(params[:id])
+  end
 end
